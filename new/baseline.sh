0b8f6e98d6721afec62419ec0cf246fe3edadcc6
Michael Orr <michael@orr.co>
2013-05-05 18:06:55 -0400

    tweaks for new setup

--- new/baseline.sh
+++ new/baseline.sh
@@ -1,22 +1,27 @@
 #! /bin/bash
 
-CURRENT_DIR=`pwd`
-MY_USER=$USER
+## Modify these to your preference
 MY_TMP_LOC="$HOME/tmp"
 MY_SRC_LOC="/usr/local/src"
 MY_OPT_LOC="/opt/dev"
+MY_DOT_LOC="/opt/dot"
+DEV_GROUP="dev"
+
+## Leave these alone
+CURRENT_DIR=`pwd`
+MY_USER=$USER
 
 ##
 # Standard groups
 ##
-TEST_VAL=`egrep -i -c "^dev" /etc/group`
-if [ $TEST_VAL = "0" ]; then 
-  echo "Creating group: dev"
-  sudo groupadd dev
+GROUP_EXISTS=`egrep -i -c "^$DEV_GROUP" /etc/group`
+if [ $GROUP_EXISTS = "0" ]; then 
+  echo "Creating group: $DEV_GROUP"
+  sudo groupadd $DEV_GROUP
 fi
-echo "Adding root and $MY_USER to group: dev"
-sudo usermod -a -G dev root
-sudo usermod -a -G dev $MY_USER
+echo "Adding root and $MY_USER to group: $DEV_GROUP"
+sudo usermod -a -G $DEV_GROUP root
+sudo usermod -a -G $DEV_GROUP $MY_USER
 
 
 ##
@@ -27,12 +32,17 @@ echo "Creating $MY_OPT_LOC..."
 sudo mkdir -p $MY_OPT_LOC
 sudo chown $MY_USER:$MY_USER $MY_OPT_LOC
 
+echo "Creating $MY_DOT_LOC..."
+sudo mkdir -p $MY_DOT_LOC
+sudo chown $MY_USER:$MY_USER $MY_DOT_LOC
+
 echo "Creating $MY_TMP_LOC..."
 sudo mkdir -p $MY_TMP_LOC
 sudo chown $MY_USER:$MY_USER $MY_TMP_LOC
+
 echo "Creating $MY_SRC_LOC..."
 sudo mkdir -p $MY_SRC_LOC
-sudo chown root:dev $MY_SRC_LOC
+sudo chown root:$DEV_GROUP $MY_SRC_LOC
 sudo chmod g+rw -R $MY_SRC_LOC
 
 ##
--- new/dev.sh
+++ new/dev.sh
@@ -4,6 +4,34 @@
 # Install some basic utils and dotfiles
 ##
 
-sudo apt-get --yes install ack-grep zsh vim git
+sudo apt-get --yes install ack-grep zsh vim git curl
+
 git config --global user.email "michael@orr.co"
 git config --global user.name "Michael Orr"
+
+## move the dot files into place first
+# git config --global core.excludesfile /opt/dot/git/gitignore_global
+
+## move the dot files into place first
+# ln -s /opt/dot/ack/ackrc ~/.ackrc
+
+## 
+# export PYTHONPATH="/usr/local/src" > /etc/profile
+
+
+######################
+# ZSH configs
+# omz
+# vimrc
+# other dotfiles
+# ack-grep versus ack alias
+
+
+## these go in .zshrc
+# if [[ -f $HOME/.zshrc.local ]]; then
+#   source $HOME/.zshrc.local 
+# fi
+#
+
+# host=${HOST%%.*}
+# [[ -x $ZDOTDIR/overrides/$host ]] && source $ZDOTDIR/overrides/$host
